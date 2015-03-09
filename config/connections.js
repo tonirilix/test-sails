89c6f76f02b19ea82ea8a0c40762c1f7300415d9
tonirilix <jesusmontes.mx@hotmail.com>
2015-03-09 01:48:48 -0400

    Configure for mongolab

--- config/connections.js
+++ config/connections.js
@@ -56,10 +56,20 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  someMongodbServer: {
+  localMongoDb: {
     adapter: 'sails-mongo',
     host: 'localhost',
     port: 27017,
+    // user: 'username',
+    // password: 'password',
+    // database: 'your_mongo_db_name_here'
+  },
+
+  MongoLabMongoDb: {
+    adapter: 'sails-mongo',
+    url: process.env.MONGOLAB_URI    
+    // host: 'localhost',
+    // port: 27017,
     // user: 'username',
     // password: 'password',
     // database: 'your_mongo_db_name_here'
